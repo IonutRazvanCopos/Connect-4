47f045a60ac839d995a1d50a267239e4b9dd0800
IonutRazvanCopos <130033085+IonutRazvanCopos@users.noreply.github.com>
2024-10-03 12:06:48 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const container = document.querySelector("#board");
 const message = document.getElementById('message');
 let gameOver = false, winR = 0, winY = 0, winner = false;
 
-const max = 42, four = 4, seven = 7, lastLine = 35;
+const max = 42, connections = 4, reper = 7, lastLine = 35;
 
 function generateTable() {
     for (let i = 0; i < max; ++i) {
@@ -17,30 +17,30 @@ function generateTable() {
 
 generateTable();
 
-let cont = container.children;
-let col = [
-    [cont[0].id, cont[7].id, cont[14].id, cont[21].id,
-    cont[28].id, cont[35].id],
-    [cont[1].id, cont[8].id, cont[15].id, cont[22].id,
-    cont[29].id, cont[36].id],
-    [cont[2].id, cont[9].id, cont[16].id, cont[23].id,
-    cont[30].id, cont[37].id],
-    [cont[3].id, cont[10].id, cont[17].id, cont[24].id,
-    cont[31].id, cont[38].id],
-    [cont[4].id, cont[11].id, cont[18].id, cont[25].id,
-    cont[32].id, cont[39].id],
-    [cont[5].id, cont[12].id, cont[19].id, cont[26].id,
-    cont[33].id, cont[40].id],
-    [cont[6].id, cont[13].id, cont[20].id, cont[27].id,
-    cont[34].id, cont[41].id]
+let piece = container.children;
+let table = [
+    [piece[0].id, piece[7].id, piece[14].id, piece[21].id,
+    piece[28].id, piece[35].id],
+    [piece[1].id, piece[8].id, piece[15].id, piece[22].id,
+    piece[29].id, piece[36].id],
+    [piece[2].id, piece[9].id, piece[16].id, piece[23].id,
+    piece[30].id, piece[37].id],
+    [piece[3].id, piece[10].id, piece[17].id, piece[24].id,
+    piece[31].id, piece[38].id],
+    [piece[4].id, piece[11].id, piece[18].id, piece[25].id,
+    piece[32].id, piece[39].id],
+    [piece[5].id, piece[12].id, piece[19].id, piece[26].id,
+    piece[33].id, piece[40].id],
+    [piece[6].id, piece[13].id, piece[20].id, piece[27].id,
+    piece[34].id, piece[41].id]
 ];
 
 function checkNr(num) {
     while (num < lastLine) {
-        num += seven;
+        num += reper;
     }
-    while (cont[num].id == "red" || cont[num].id == "yellow") {
-        num -= seven;
+    while (piece[num].id == "red" || piece[num].id == "yellow") {
+        num -= reper;
     }
     return num;
 }
@@ -58,11 +58,11 @@ function setPiece() {
         } else {
             once = 1;
         }
-        for (let line = 0; line <= seven && once == 0; ++line) {
-            for (let column = 0; column < seven && once == 0; ++column) {
-                if (e.id == col[line][column]) {
-                    cont[num].style.backgroundColor = currentPlayer;
-                    cont[num].id = `${currentPlayer}`;
+        for (let line = 0; line <= sereperven && once == 0; ++line) {
+            for (let column = 0; column < reper && once == 0; ++column) {
+                if (e.id == table[line][column]) {
+                    piece[num].style.backgroundColor = currentPlayer;
+                    piece[num].id = `${currentPlayer}`;
                     changeColor();
                     checkWin();
                     once = 1;
@@ -82,13 +82,13 @@ function changeColor() {
 }
 
 let fourCheckings = [7, 1, 8, 6];
-let begin = [
+let beginCalculation = [
     [0, 1, 2, 3, 4, 5, 6],
     [0, 7, 14, 21, 28, 35],
     [14, 7, 0, 1, 2, 3],
     [3, 4, 5, 6, 13, 20]
 ];
-let stop = [
+let stopCalculating = [
     [35, 36, 37, 38, 39, 40, 41],
     [6, 13, 20, 27, 34, 41],
     [38, 39, 40, 41, 34, 27],
@@ -96,7 +96,7 @@ let stop = [
 ];
 
 function haveWinner(a, b) {
-    if (a == four || b == four) {
+    if (a == connections || b == connections) {
         gameOver = true;
         if (!winner) {
             changeColor();
@@ -109,18 +109,18 @@ function haveWinner(a, b) {
 function checkWin() {
     let r = "red", y = "yellow";
     let columns = 7;
-    for (let target = 0; target < four; ++target) {
+    for (let target = 0; target < connections; ++target) {
         if (target > 0) {
             columns = 6;
         }
-        for (let col = 0; col < columns; ++col) {
-            for (let i = begin[target][col]; i <= stop[target][col]; i += fourCheckings[target]) {
-                if (cont[i].id == r) {
+        for (let table = 0; table < columns; ++table) {
+            for (let i = beginCalculation[target][table]; i <= stopCalculating[target][table]; i += fourCheckings[target]) {
+                if (piece[i].id == r) {
                     ++winR;
                 } else {
                     winR = 0;
                 }
-                if (cont[i].id == y) {
+                if (piece[i].id == y) {
                     ++winY;
                 } else {
                     winY = 0;
